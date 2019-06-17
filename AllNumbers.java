7bad51e6a397aeb1aefa1d7c933dc408f9a02465
mike1806 <44318554+mike1806@users.noreply.github.com>
2019-06-17 20:31:33 +0100

    Add files via upload

---
+++ AllNumbers.java
@@ -0,0 +1,24 @@
+/*
+MBorkowski
+Counter.java
+30 12 2018
+*/
+
+import javax.swing.*; //JPOptionPane
+
+public class AllNumbers{
+	public static void main(String args[]){
+
+	//Display numbers from 0 to 10 inclusive
+
+	int sum=0;
+
+for(int m=0;m<=10;m=m+1){
+	sum=sum+m;
+
+System.out.println(sum);
+}
+
+	}
+
+}
